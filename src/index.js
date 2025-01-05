9e8c16d5afb152d81ded3fffc2ab465ebe23dfee
João Victor <129808291+Joao-VictorCm@users.noreply.github.com>
2025-01-05 19:36:16 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//1. Aplicar estilos CSS ao componente App.jsx
+//para corresponder à aparência do aplicativo concluído:
+//https://c6fkx.csb.app/
+//2. Extraia o cartão de contato como um componente de cartão reutilizável.
+//3. Use adereços para renderizar o cartão de contato padrão da Beyoncé
+//para que o componente Cartão possa ser reutilizado para outros contatos.
+//4. Importe o arquivo Contacts.js para criar componentes de cartão.
